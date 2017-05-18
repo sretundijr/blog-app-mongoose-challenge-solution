d825734b994311794726a90c603827ef50a27bed
sretundijr <sretundijr@gmail.com>
2017-05-17 22:32:58 -0600

    seed date created and server control

---
+++ test/blog-app-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const { BlogPost } = require('../models');
+const { runServer, app, closeServer } = require('../server');
+
+const { TEST_DATABASE_URL } = require('../config');
+
+chai.use(chaiHttp);
+
+function createFakeBlogPost() {
+    return {
+        author: {
+            firstName: `${faker.name.firstName()}`,
+            lastName: `${faker.name.lastName()}`
+        },
+        title: `${faker.random.catch_phrase_descriptor} ${faker.random.catch_phrase_noun}`,
+        content: `${faker.lorem.paragraph}`,
+        date: Date.now()
+    }
+}
+
+function seedDatabase() {
+    const seedData = [];
+
+    for (var i = 0; i < 5; i++) {
+        seedData.push(createFakeBlogPost());
+    }
+
+    return BlogPost.insertMany(seedData);
+}
+
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
+
